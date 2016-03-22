ad7c307a18158a0510263dd380ce45faac0a1124
Christopher <christopherpigden@192-168-1-11.tpgi.com.au>
2016-03-22 20:56:50 +1100

    Removed commented out code from test setup
    Removed commented out code from test setup

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/setup/LocalSetup.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/setup/LocalSetup.java
@@ -17,39 +17,13 @@ public abstract class LocalSetup {
     public WebDriver driver;
     public String baseUrl;
     protected Eyes eyes;
-    protected String testName;
-    public String proxy = System.getenv("PROXYURL");
-
-//    protected MatchLevel getMatchLevel() {
-//        String matchLevel = System.getenv("MATCH_LEVEL");
-//        return MatchLevel.valueOf(matchLevel);
-//    }
-//
-//    protected RectangleSize getRectangleSize() {
-//        String rectangleHeight = System.getenv("RECTANGLE_HEIGHT");
-//        String rectangleWidth = System.getenv("RECTANGLE_WIDTH");
-//        return new RectangleSize(Integer.parseInt(rectangleWidth), Integer.parseInt(rectangleHeight));
-//    }
-
+    
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = System.getenv("BASEURL");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//        eyes = createApliToolsEyes();
-//        this.driver = eyes.open(driver, "DoE", testName, getRectangleSize());
     }
-
-//    private Eyes createApliToolsEyes() {
-//        final Eyes eyes = new Eyes();
-//        eyes.setApiKey(System.getenv("APPLITOOLS.APIKEY"));
-//        eyes.setMatchLevel(getMatchLevel());
-//        eyes.setForceFullPageScreenshot(true);
-//        if(proxy != null) {
-//            eyes.setProxy(new ProxySettings(proxy));
-//        }
-//        return eyes;
-//    }
 
     @After
     public void tearDown() throws Exception {
