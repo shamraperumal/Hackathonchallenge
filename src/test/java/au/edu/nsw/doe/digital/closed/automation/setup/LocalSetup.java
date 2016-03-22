f0223ba93c8a4f520425b9f12643882518951c03
Christopher <christopherpigden@192-168-1-11.tpgi.com.au>
2016-03-22 20:52:36 +1100

    Commented out a chunk of the local test set up code
    Troubleshooting test run issues

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/setup/LocalSetup.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/setup/LocalSetup.java
@@ -1,9 +1,6 @@
 package au.edu.nsw.doe.digital.closed.automation.setup;
 
 import com.applitools.eyes.Eyes;
-import com.applitools.eyes.MatchLevel;
-import com.applitools.eyes.ProxySettings;
-import com.applitools.eyes.RectangleSize;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -23,36 +20,36 @@ public abstract class LocalSetup {
     protected String testName;
     public String proxy = System.getenv("PROXYURL");
 
-    protected MatchLevel getMatchLevel() {
-        String matchLevel = System.getenv("MATCH_LEVEL");
-        return MatchLevel.valueOf(matchLevel);
-    }
-
-    protected RectangleSize getRectangleSize() {
-        String rectangleHeight = System.getenv("RECTANGLE_HEIGHT");
-        String rectangleWidth = System.getenv("RECTANGLE_WIDTH");
-        return new RectangleSize(Integer.parseInt(rectangleWidth), Integer.parseInt(rectangleHeight));
-    }
+//    protected MatchLevel getMatchLevel() {
+//        String matchLevel = System.getenv("MATCH_LEVEL");
+//        return MatchLevel.valueOf(matchLevel);
+//    }
+//
+//    protected RectangleSize getRectangleSize() {
+//        String rectangleHeight = System.getenv("RECTANGLE_HEIGHT");
+//        String rectangleWidth = System.getenv("RECTANGLE_WIDTH");
+//        return new RectangleSize(Integer.parseInt(rectangleWidth), Integer.parseInt(rectangleHeight));
+//    }
 
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = System.getenv("BASEURL");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        eyes = createApliToolsEyes();
-        this.driver = eyes.open(driver, "DoE", testName, getRectangleSize());
+//        eyes = createApliToolsEyes();
+//        this.driver = eyes.open(driver, "DoE", testName, getRectangleSize());
     }
 
-    private Eyes createApliToolsEyes() {
-        final Eyes eyes = new Eyes();
-        eyes.setApiKey(System.getenv("APPLITOOLS.APIKEY"));
-        eyes.setMatchLevel(getMatchLevel());
-        eyes.setForceFullPageScreenshot(true);
-        if(proxy != null) {
-            eyes.setProxy(new ProxySettings(proxy));
-        }
-        return eyes;
-    }
+//    private Eyes createApliToolsEyes() {
+//        final Eyes eyes = new Eyes();
+//        eyes.setApiKey(System.getenv("APPLITOOLS.APIKEY"));
+//        eyes.setMatchLevel(getMatchLevel());
+//        eyes.setForceFullPageScreenshot(true);
+//        if(proxy != null) {
+//            eyes.setProxy(new ProxySettings(proxy));
+//        }
+//        return eyes;
+//    }
 
     @After
     public void tearDown() throws Exception {
