2ee7bdd09370f49c3023e42a0ac1752e0b3bd56d
Christopher <christopherpigden@0826-N03444-MacBook-Pro.local>
2016-06-06 16:12:00 +1000

    Updated the pom file and get website test
    Updated the pom file and get website test

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizlegacytemplates/desktop/SquizGefWebsiteTemplateTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizlegacytemplates/desktop/SquizGefWebsiteTemplateTest.java
@@ -23,6 +23,9 @@ public class SquizGefWebsiteTemplateTest extends CapabilitiesBuilder {
         squizAction.getPage("gef-website-template-base", "gef-website-template-landingpage");
         eyes.checkWindow(System.getenv("EYES-GEF-WEBSITE-LANDING"));
 
+        squizAction.getPage("gef-website-template-base", "gef-website-template-digitalfoundationspage");
+        eyes.checkWindow(System.getenv("EYES-GEF-WEBSITE-DIGITAL-FOUNDATIONS"));
+
         squizAction.getPage("gef-website-template-base", "gef-website-template-newsindexpage");
         eyes.checkWindow(System.getenv("EYES-GEF-WEBSITE-NEWS-INDEX"));
 
