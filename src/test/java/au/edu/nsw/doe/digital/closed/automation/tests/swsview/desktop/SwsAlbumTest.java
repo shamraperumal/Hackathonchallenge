1596af71557d87effe237763e9365c97add39a0f
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2016-11-03 12:28:09 +1100

    AT-26 - Small update to SwsAlbumTest url

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/swsview/desktop/SwsAlbumTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/swsview/desktop/SwsAlbumTest.java
@@ -21,37 +21,37 @@ public class SwsAlbumTest extends CapabilitiesBuilder {
     public void swsAlbumTest() throws Exception {
         {
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-1", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-1", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 1 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-2", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-2", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 2 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-3", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-3", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 3 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-4", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-4", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 4 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-5", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-5", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 5 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-6", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-6", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 6 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-7", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-7", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 7 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-8", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-8", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 8 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-9", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-9", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 9 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-10", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-10", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 10 - Album");
 
-            action.getPage("SWS-BASE-VIEW", "sws-design-11", "album-1/", "");
+            action.getPage("SWS-BASE-VIEW", "sws-design-11", "gallery/album-1/", "");
             eyes.checkWindow("SWS Design 11 - Album");
 
             eyes.close();
