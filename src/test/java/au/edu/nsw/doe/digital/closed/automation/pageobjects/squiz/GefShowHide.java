fcd95a71292a85a73dbaab4bb40b317db9a795ab
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-01-16 14:03:31 +1100

    MAT-30 - Added a new test class for the GefShowHideTest . Updated the page objects for the GefShowHide.

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefShowHide.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefShowHide.java
@@ -10,5 +10,6 @@ public class GefShowHide {
     public static By showHide1 = (By.xpath("//h3[@id='at136164']"));
     public static By showHide2 = (By.xpath("//h3[@id='at136168']"));
     public static By showHide3 = (By.xpath("//h3[@id='at136172']"));
+    public static By showHideContent1 = (By.xpath("//div[@class='uk-accordion-content uk-active']"));
 
 }
