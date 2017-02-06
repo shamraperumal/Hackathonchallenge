bc656a7497bce1d3e246aedf345e1c6db6ef057b
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-02-06 14:11:44 +1100

    MAT-46 Added page objects for the global header and footer.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefGlobalFooter.java
@@ -0,0 +1,12 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by christopherpigden on 6/2/17.
+ */
+public class GefGlobalFooter {
+    public static By searchField = By.xpath("//img[@title='Department of Education']");
+
+    
+}
