0e8a0ed3de04dc993d6ce9080aec5f37049d532e
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-02-07 14:56:19 +1100

    MAT-47 - Fixed an issue with the local set up class. Fixed an issue with global footer cpageobject. Added a step to the hero banner test.

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefGlobalFooter.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefGlobalFooter.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
  * Created by christopherpigden on 6/2/17.
  */
 public class GefGlobalFooter {
-    public static By searchField = By.xpath("//img[@title='Department of Education']");
+    public static By doELogo = By.xpath("//img[@title='Department of Education']");
 
     
 }
--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizuat/desktop/interactions/GefHeroBannerTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizuat/desktop/interactions/GefHeroBannerTest.java
@@ -1,7 +1,9 @@
 package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizuat.desktop.interactions;
 
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefGlobalFooter;
 import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefHomepage;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.setup.LocalSetup;
 import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +11,7 @@ import org.junit.Test;
 /**
  * Created by christopherpigden on 16/1/17.
  */
-public class GefHeroBannerTest extends CapabilitiesBuilder {
+public class GefHeroBannerTest extends LocalSetup {
 
 
     @Test
@@ -19,6 +21,7 @@ public class GefHeroBannerTest extends CapabilitiesBuilder {
 
         squizAction.getPage("gef-education-base", "");
         squizAction.clickWebElement(GefHomepage.heroBannerButton);
+        squizAction.wait(GefGlobalFooter.doELogo);
         Assert.assertEquals("https://uat.education.nsw.gov.au/test-automation/lorem-ipsum",driver.getCurrentUrl());
 
 
