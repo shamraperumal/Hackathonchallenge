cca6c498a090287cb3e259fdfb47f81fd452719a
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-02-09 15:55:29 +1100

    MAT-41 - Added page objects for EdConnect and a new test.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefEdConnect.java
@@ -0,0 +1,11 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by christopherpigden on 7/2/17.
+ */
+public class GefEdConnect {
+    public static By edConnectButton = By.id("drawer84tab");
+    public static By edconnectDrawer = By.xpath("//a[@aria-expanded='true']");
+}
---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizuat/desktop/interactions/GefEdConnectDrawerTest.java
@@ -0,0 +1,36 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizuat.desktop.interactions;
+
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefEdConnect;
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefHomepage;
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.setup.LocalSetup;
+import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 7/2/17.
+ */
+public class GefEdConnectDrawerTest extends CapabilitiesBuilder{
+    @Test
+    public void gefEducationDesktopEdConnectDrawerTest() throws Exception {
+
+        SquizActionMethods squizAction = new SquizActionMethods(driver);
+
+        squizAction.getPage("gef-education-base", "");
+//        squizAction.maximiseWindow();
+//        driver.get("https://uat.education.nsw.gov.au/test-automation");
+        squizAction.clickWebElement(GefEdConnect.edConnectButton);
+        try{
+            String ariaExpanded = driver.findElement(GefEdConnect.edConnectButton).getAttribute("aria-expanded");
+            Assert.assertEquals("true", ariaExpanded);
+            eyes.checkRegion(GefEdConnect.edconnectDrawer);
+            System.out.println("Aria expanded ="+ariaExpanded);
+
+        }catch (final Exception e){
+            System.out.println("Ed Connect is not expanded");
+            throw e;
+        }
+
+        }
+}
