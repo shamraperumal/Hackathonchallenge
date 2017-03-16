c5efc1485b49de09b17662ca6c4e8522207e3466
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-03-16 12:10:42 +1100

    MAT-59 - Fixed issue with default elements page objects for links_auto.

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/whitelabel/DefaultElements.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/whitelabel/DefaultElements.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
  * Created by christopherpigden on 1/3/17.
  */
 public class DefaultElements {
-    public static By linkList = By.id("linklist_auto");
+    public static By linkList = By.id("links_auto");
     public static By typography = By.id("typography_auto");
     public static By blockQuote = By.id("blockquote_auto");
     public static By iFrame = By.id("iframe_auto");
