53fdc735d9937d890280b03950e44bbc300fdcb3
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-03-23 14:19:36 +1100

    REP-644 - Added tests for grey theme on non prod.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizgreytheme/desktop/SquizGreyThemeTemplateTest.java
@@ -0,0 +1,7 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizgreytheme.desktop;
+
+/**
+ * Created by christopherpigden on 21/3/17.
+ */
+public class SquizGreyThemeTemplateTest {
+}
