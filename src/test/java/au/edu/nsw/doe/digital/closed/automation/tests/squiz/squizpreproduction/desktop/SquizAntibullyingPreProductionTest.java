4eda248f57d85d2d235175ed1ecc2ba0d7a02384
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-06-29 14:26:58 +1000

    MAT-77 - Added tests for pre-prod and prod desktop and mobile anti bullying sites

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizpreproduction/desktop/SquizAntibullyingPreProductionTest.java
@@ -0,0 +1,33 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizpreproduction.desktop;
+
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 9/09/2015.
+ */
+public class SquizAntibullyingPreProductionTest extends CapabilitiesBuilder {
+
+    @Test
+    public void antibullyingDesktopPreProductionEyesTest() throws Exception {
+
+        SquizActionMethods squizAction = new SquizActionMethods(driver);
+
+        squizAction.getPage("antibullying-base", "antibullying-home");
+        eyes.checkWindow(System.getenv("EYES-ANTIBULLYING-HOME"));
+
+        squizAction.getPage("antibullying-base", "antibullying-landing");
+        eyes.checkWindow(System.getenv("EYES-ANTIBULLYING-LANDING"));
+
+        squizAction.getPage("antibullying-base", "antibullying-standard");
+        eyes.checkWindow(System.getenv("EYES-ANTIBULLYING-Standard"));
+
+        squizAction.getPage("antibullying-base", "antibullying-catalogue");
+        eyes.checkWindow(System.getenv("EYES-ANTIBULLYING-CATALOGUE"));
+
+
+        eyes.close();
+    }
+}
