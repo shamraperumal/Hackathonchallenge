67c1b8c7781ce88fd52ef4208b4ea9d869ba9541
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-07-19 14:34:07 +1000

    MAT-72 - Added junit @Before to test

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizpreproduction/desktop/SquizEducationHubsPreProductionTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizpreproduction/desktop/SquizEducationHubsPreProductionTest.java
@@ -2,7 +2,9 @@ package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizpreproduction.
 
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
 import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActionMethods;
 import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActions;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,6 +13,9 @@ import org.junit.Test;
 public class SquizEducationHubsPreProductionTest extends CapabilitiesBuilder {
 
     private PwsActions action;
+
+    @Before
+    public void doSetUp() { action = new PwsActionMethods(driver);}
 
     @Test
     public void educationHubsDesktopPreProductionEyesTest() throws Exception {
