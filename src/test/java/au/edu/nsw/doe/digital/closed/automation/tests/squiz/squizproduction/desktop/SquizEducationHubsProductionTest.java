2c0bbf091510e81c214373b4e2428a2a766a54e5
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-07-21 15:09:02 +1000

    MAT-72 - Added hub tests to production test suite

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizproduction/desktop/SquizEducationHubsProductionTest.java
@@ -0,0 +1,40 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizproduction.desktop;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActions;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 9/09/2015.
+ */
+public class SquizEducationHubsProductionTest extends CapabilitiesBuilder {
+
+    private PwsActions action;
+
+    @Before
+    public void doSetUp() { action = new PwsActionMethods(driver);}
+
+    @Test
+    public void educationHubsDesktopProductionEyesTest() throws Exception {
+
+        SquizActionMethods squizAction = new SquizActionMethods(driver);
+        
+        squizAction.getPage("education-base", "");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-HOME"));
+
+        action.ssoLoginEntry();
+
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-PRIVATE-PRIMARY-HUB"));
+
+        squizAction.getPage("education-base", "education-primary-hub");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-PRIVATE-PRIMARY-HUB"));
+
+        squizAction.getPage("education-base", "education-secondary-hub");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-SECONDARY-HUB"));
+
+        eyes.close();
+    }
+    }
---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizproduction/mobile/SquizEducationHubsMobileProductionTest.java
@@ -0,0 +1,31 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizproduction.mobile;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 9/09/2015.
+ */
+public class SquizEducationHubsMobileProductionTest extends CapabilitiesBuilder {
+
+    @Test
+    public void educationHubsMobileProductionEyesTest() throws Exception {
+
+        SquizActionMethods squizAction = new SquizActionMethods(driver);
+
+        squizAction.getPage("education-base", "");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-HOME"));
+
+        squizAction.ssoLoginEntry();
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-PRIVATE-PRIMARY-HUB"));
+
+        squizAction.getPage("education-base", "education-primary-hub");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-PRIVATE-PRIMARY-HUB"));
+
+        squizAction.getPage("education-base", "education-secondary-hub");
+        eyes.checkWindow(System.getenv("EYES-EDUCATION-SECONDARY-HUB"));
+
+        eyes.close();
+    }
+    }
