e9e62c526f2d61938828b1aa22ab95d21aab8789
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-08-11 17:46:53 +1000

    SWR-308-set-up-an-area-for-horizon-tests - Added page objects for global footer and updated test to check global footer region only

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swswhitelabel/GlobalElements.java
@@ -0,0 +1,12 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by christopherpigden on 11/8/17.
+ */
+public class GlobalElements {
+public static By globalFooter = By.id("globalfooter");
+}
+
+
--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/GlobalElementsTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/GlobalElementsTest.java
@@ -1,5 +1,6 @@
 package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
 
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.GlobalElements;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
 import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActionMethods;
 import au.edu.nsw.doe.digital.closed.automation.tests.swsedit.PwsActions;
@@ -21,7 +22,7 @@ public class GlobalElementsTest extends CapabilitiesBuilder {
     public void someTest() throws Exception {
         {
             action.getPage("sws-component-base","sws-global-footer");
-            eyes.checkWindow("");
+            eyes.checkRegion(GlobalElements.globalFooter);
 
             eyes.close();
         }
