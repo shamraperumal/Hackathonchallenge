9cffdf98099fe46096d0f8328a4ab671900c42f9
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-08-18 12:51:37 +1000

    SWR-340 - Renamed class BreadcrumbsTest to BreadcrumbTest because the build is failing for stupid reasons.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/BreadcrumbTest.java
@@ -0,0 +1,29 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
+
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.GlobalElements;
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 8/8/17.
+ */
+public class BreadcrumbTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void breadcrumbsTest() throws Exception {
+        {
+            action.setScreenDimensions("device");
+            action.getPage("sws-component-base","sws-breadcrumbs");
+            eyes.checkRegion(GlobalElements.breadcrumbs);
+
+            eyes.close();
+        }
+    }
+}
