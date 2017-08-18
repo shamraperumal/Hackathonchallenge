fc7719d765fca565ebf8469d116910d8002c40da
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-08-18 14:53:37 +1000

    SWR-340 - Adding the setScreenDimensions code to the test class instead actions class to see if it makes a difference

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/breadcrumbsTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/breadcrumbsTest.java
@@ -3,6 +3,7 @@ package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
 import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.GlobalElements;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 
 /**
  * Created by christopherpigden on 8/8/17.
@@ -18,12 +19,27 @@ public class breadcrumbsTest extends CapabilitiesBuilder {
 
     @Test
     public void breadcrumbsTest() throws Exception {
-        {
-            action.setScreenDimensions();
+
+            try {
+                String thisDevice = System.getenv("DEVICE");
+                if(thisDevice == "Tablet"){
+                    Dimension dimension = new Dimension(768, 946);
+                    driver.manage().window().setSize(dimension);
+                    System.out.println("Set tablet width");
+                }
+                if(thisDevice=="Mobile"){
+                    Dimension dimension = new Dimension(375, 650);
+                    driver.manage().window().setSize(dimension);
+                    System.out.println("Set mobile width");
+                }
+            } catch (final Exception e) {
+                System.out.println("Failed to set screen dimensions");
+            }
+
             action.getPage("sws-component-base","sws-breadcrumbs");
             eyes.checkRegion(GlobalElements.breadcrumbs);
 
             eyes.close();
         }
     }
-}
+
