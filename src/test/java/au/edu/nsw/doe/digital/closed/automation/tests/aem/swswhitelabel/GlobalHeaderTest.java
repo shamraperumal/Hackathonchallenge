6f73a04a855d4a56f829a80069d9334933b49e06
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-08-21 16:28:42 +1000

    SWR-269 - Added page object and test class for global header.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/GlobalHeaderTest.java
@@ -0,0 +1,29 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
+
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.GlobalElements;
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 21/8/17.
+ */
+public class GlobalHeaderTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void globalHeaderTest() throws Exception {
+        {
+            action.setScreenDimensions();
+            action.getPage("sws-component-base","sws-global-header");
+            eyes.checkRegion(GlobalElements.globalHeader);
+
+            eyes.close();
+        }
+    }
+}
