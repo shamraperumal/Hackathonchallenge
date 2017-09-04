5bac5b11c874b55ebd88a6886aa6ddfcacac4eb0
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-09-04 17:31:43 +1000

    SWR-406 - Added a test for the standard page template.

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/StandardPageTest.java
@@ -0,0 +1,29 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
+
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.GlobalElements;
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import org.junit.Test;
+
+/**
+ * Created by christopherpigden on 8/8/17.
+ */
+public class StandardPageTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void standardPageTest() throws Exception {
+        {
+            action.setScreenDimensions();
+            action.getPage("sws-component-base","sws-standard");
+            eyes.checkWindow(System.getenv("Whitelabel standard page template"));
+
+            eyes.close();
+        }
+    }
+}
