a63832c19718c5545bd36007cb8a27bc22975391
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-09-11 09:19:45 +1000

    SWR-270 - Updated page object for mega menu

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swswhitelabel/GlobalElements.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swswhitelabel/GlobalElements.java
@@ -21,7 +21,7 @@ public class GlobalElements {
     public static By subMenuLink1 = By.cssSelector(".uk-parent:nth-child(4) .sws-meganav-megalinks");
     public static By subMenuLink2 = By.cssSelector(".uk-parent:nth-child(5) .sws-meganav-megalinks");
     public static By subMenuLink3 = By.cssSelector(".uk-parent:nth-child(7) .sws-meganav-megalinks");
-    public static By subMenu = By.id(".uk-margin-top");
+    public static By subMenu = By.cssSelector(".uk-dropdown-bottom");
 
 }
 
