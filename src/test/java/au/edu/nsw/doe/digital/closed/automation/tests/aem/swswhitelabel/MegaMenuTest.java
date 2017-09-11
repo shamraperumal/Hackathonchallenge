1b2744bde369cbf57d4088882f9dd2f5ce5ccef1
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-09-11 12:05:58 +1000

    SWR-270 - Mega menu fixes

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/MegaMenuTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/MegaMenuTest.java
@@ -24,8 +24,6 @@ public class MegaMenuTest extends CapabilitiesBuilder {
             action.click(GlobalElements.subMenuLink1);
             eyes.checkRegion(GlobalElements.subMenu);
 
-            action.getPage("sws-component-base","sws-mega-menu");
-
             action.click(GlobalElements.subMenuLink2);
             eyes.checkRegion(GlobalElements.subMenu);
 
