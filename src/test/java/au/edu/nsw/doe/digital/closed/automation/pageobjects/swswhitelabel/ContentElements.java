cdabc9457ea279e7e3c44c1d4ba31d453a5e776c
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-10-09 17:45:22 +1100

    SWR-589 - Updated with new test for content template and the show hide and tabs interactions

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swswhitelabel/ContentElements.java
@@ -0,0 +1,17 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by christopherpigden on 11/8/17.
+ */
+public class ContentElements {
+
+    //Clickable web elements
+    public static By showHide = By.id("at443");
+    public static By showHideComponent = By.cssSelector(".uk-active");
+    public static By tab = By.id("label_tabs294");
+    public static By tabComponent = By.cssSelector(".gef-tabs--active :nth-child(1)");
+}
+
+
---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/ContentTemplateTest.java
@@ -0,0 +1,39 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
+
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.ContentElements;
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel.ImageElements;
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import org.junit.Test;
+
+public class ContentTemplateTest extends CapabilitiesBuilder{
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+   
+    @Test
+    public void contentTemplateTest()throws Exception {
+
+        action.setScreenDimensions();
+
+        action.getPage("sws-component-base","sws-content");
+
+        eyes.checkWindow(System.getenv("sws-content"));
+
+        action.click(ContentElements.showHide);
+
+        eyes.checkRegion(ContentElements.showHideComponent);
+
+        action.click(ContentElements.tab);
+
+        eyes.checkRegion(ContentElements.tabComponent);
+
+        eyes.close();
+
+    }
+}
+
