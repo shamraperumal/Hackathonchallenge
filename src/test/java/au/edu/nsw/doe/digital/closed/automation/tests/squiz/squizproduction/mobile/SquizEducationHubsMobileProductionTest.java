b74b60b139b622a8692262bf24b0fb0e8c44e464
Kristina Mason <kristina.mason@det.nsw.edu.au>
2017-10-12 12:42:06 +1100

    MAT-82v2 updating inside the department test to wait for the search icon instead of the title

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizproduction/mobile/SquizEducationHubsMobileProductionTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizproduction/mobile/SquizEducationHubsMobileProductionTest.java
@@ -1,8 +1,11 @@
 package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizproduction.mobile;
 
+import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefInsideTheDepartment;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
 import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by christopherpigden on 9/09/2015.
@@ -14,10 +17,12 @@ public class SquizEducationHubsMobileProductionTest extends CapabilitiesBuilder 
 
         SquizActionMethods squizAction = new SquizActionMethods(driver);
 
+
         squizAction.getPage("education-base", "");
         eyes.checkWindow(System.getenv("EYES-EDUCATION-HOME"));
 
         squizAction.ssoLoginEntry();
+        squizAction.wait(GefInsideTheDepartment.searchButton);
         eyes.checkWindow(System.getenv("EYES-EDUCATION-PRIVATE-PRIMARY-HUB"));
 
         squizAction.getPage("education-base", "education-primary-hub");
