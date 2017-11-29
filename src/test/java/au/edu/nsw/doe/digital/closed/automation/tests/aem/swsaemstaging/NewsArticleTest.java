347924471241ea6c7489e5a7984f570f97c7f3b2
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2017-11-29 17:50:48 +1100

    SWR-1014 - Added tests for the AEM integrated site on staging

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swsaemstaging/NewsArticleTest.java
@@ -0,0 +1,32 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swsaemstaging;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActions;
+import org.junit.Test;
+
+
+public class NewsArticleTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void NewsIndexTest() throws Exception {
+        {
+            action.setScreenDimensions();
+
+            action.getPage("sws-aem-base-1","homepage-1");
+            eyes.checkWindow(System.getenv("sws-aem-homepage-1"));
+
+            action.getPage("sws-aem-base-1","homepage-2");
+            eyes.checkWindow(System.getenv("sws-aem-homepage-2"));
+
+            eyes.close();
+        }
+    }
+}
---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swsaemstaging/StandardContentPageTest.java
@@ -0,0 +1,30 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swsaemstaging;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActions;
+import org.junit.Test;
+
+
+public class StandardContentPageTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void StandardContentPageTest() throws Exception {
+        {
+            action.setScreenDimensions();
+
+            action.getPage("sws-aem-base-1","homepage-1");
+            eyes.checkWindow(System.getenv("sws-aem-homepage-1"));
+
+
+            eyes.close();
+        }
+    }
+}
