0da08e70268ab6014844454337cf97d124189dff
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2018-01-16 07:48:03 +1000

    SWR-1292 - Added album and event detail tests

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swswhitelabel/AlbumDetailElements.java
@@ -0,0 +1,18 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.swswhitelabel;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by christopherpigden on 11/8/17.
+ */
+public class AlbumDetailElements {
+
+    //Clickable web elements
+    public static By image1 = By.xpath("//img[@alt='This is the image caption for number 1']");
+    public static By next = By.xpath("//button[@class='fancybox-button fancybox-button--arrow_right']");
+
+
+
+}
+
+
---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/aem/swswhitelabel/EventDetailTest.java
@@ -0,0 +1,31 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.aem.swswhitelabel;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActionMethods;
+import au.edu.nsw.doe.digital.closed.automation.tests.aem.AemActions;
+import org.junit.Test;
+
+
+public class EventDetailTest extends CapabilitiesBuilder {
+
+    private AemActions action;
+
+    @Override
+    public void doSetUp() {
+        action = new AemActionMethods(driver);
+    }
+
+    @Test
+    public void eventDetailTest() throws Exception {
+        {
+            action.setScreenDimensions();
+            action.getPage("sws-component-base","event-detail");
+            eyes.checkWindow(System.getenv("sws-event-detail"));
+
+            action.getPage("sws-component-base","event-detail-new");
+            eyes.checkWindow(System.getenv("sws-event-detail-new"));
+
+            eyes.close();
+        }
+    }
+}
