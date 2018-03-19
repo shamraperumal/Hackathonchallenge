038cf737a47a672b9b4898d50b88a266947ef675
Christopher Pigden <chris.pigden@det.nsw.edu.au>
2018-03-19 17:47:09 +1100

    SWR-1014 - Updated page object for mobile mega menu to use xpaths.

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swsaem/GlobalElements.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/swsaem/GlobalElements.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class GlobalElements {
 
     public static By megaMenu = By.xpath("//div[@class='sws-mega-menu uk-visible-large']");
-    public static By megaMobileMenu = By.xpath("//div[@id='mobile-nav-container']");
+    public static By megaMobileMenu = By.xpath("//nav[@class='uk-width-1-1 uk-container uk-container-center uk-flex uk-flex-center uk-flex-space-between']");
 
 
     //Clickable web elements
@@ -14,6 +14,6 @@ public class GlobalElements {
     public static By subMenuLink2 = By.cssSelector(".uk-parent:nth-child(5) .sws-meganav-megalinks");
     public static By subMenuLink3 = By.cssSelector(".uk-parent:nth-child(7) .sws-meganav-megalinks");
     public static By subMenu = By.cssSelector(".uk-dropdown-bottom");
-    public static By mobileMegaMenu = By.cssSelector(".sws-local-mobile-nav__icon--nav");
+    public static By mobileMegaMenu = By.xpath("//button[@class='uk-navbar-toggle sws-local-mobile-nav__icon--nav']");
     public static By mobileNavExpanded = By.id("mobile-nav-container");
 }
