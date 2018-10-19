4cd2c50240d58794ce36279a21140db2f9d9c529
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-10-19 12:03:22 +1100

    Moved School infrastructure mobile test case to deprecated

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizdepricatedtests/SquizSchoolInfrastructureMobilePreProductionTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/squizdepricatedtests/SquizSchoolInfrastructureMobilePreProductionTest.java
@@ -1,4 +1,4 @@
-package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizpreproduction.mobile;
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.squizdepricatedtests;
 
 
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
