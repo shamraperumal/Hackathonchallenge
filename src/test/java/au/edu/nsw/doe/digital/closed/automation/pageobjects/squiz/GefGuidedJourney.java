db2450da0d515709837bf10453f254748e7101be
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-10-22 17:04:47 +1100

    Added new elements for Guided Journey page - Desktop view

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefGuidedJourney.java
@@ -0,0 +1,14 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by ShravanNagareddi on 22/10/18.
+ * Adding elements
+ */
+public class GefGuidedJourney {
+    public static By previousButton = By.className("uk-button gef-button gef-button--alt1 gef-button-x-large gef-pagination__prev");
+    public static By nextButton = By.className("uk-button gef-button gef-button-x-large gef-pagination__next");
+    public static By goToSectionButton = By.className("uk-button gef-button");
+}
+
