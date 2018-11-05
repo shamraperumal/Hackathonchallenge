6b1436e0583e7d18d27e92f413a61b689655711a
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-11-05 16:40:49 +1100

    Added test cases for feedback

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/Preprodimprovements/ProdFeedbackButton/SquizFeedbackProductionTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/Preprodimprovements/ProdFeedbackButton/SquizFeedbackProductionTest.java
@@ -1,39 +1,31 @@
-package au.edu.nsw.doe.digital.closed.automation.tests.squiz.Preprodimprovements.Policy;
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.Preprodimprovements.ProdFeedbackButton;
 
 import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefSearch;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
 import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
 import org.junit.Test;
 
-public class SquizPoliciesPreProductionTest extends CapabilitiesBuilder {
+public class SquizFeedbackProductionTest extends CapabilitiesBuilder {
 
     @Test
     public void gefPoliciesDesktopPreProductionEyesTest() throws Exception {
 
         SquizActionMethods squizAction = new SquizActionMethods(driver);
-        squizAction.getPage("gef-policies-base", "gef-policies-search");
+        squizAction.getPage("gef-feedback-base", "gef-feedback-department-contacts");
         System.out.println("Search page is launched succesfully");
         Thread.sleep(2000);
 
-        squizAction.getPage("gef-policies-base", "");
+        squizAction.getPage("gef-feedback-base", "gef-feedback-parents-carers");
 
         System.out.println("Search page is launched succesfully");
 
        // squizAction.getPage("gef-policies-base", "gef-policies-lms");
        // squizAction.getPage("gef-policies-base", "gef-policies-AZ");
 
-        squizAction.getPage("gef-policies-base", "gef-policies-topic");
-
-        squizAction.getPage("gef-policies-base", "gef-policies-Policy");
-
-        squizAction.getPage("gef-policies-base", "gef-policies-document-history");
-
-        squizAction.getPage("gef-policies-base", "gef-policies-private-policy");
-
        // Added below to wait until all the search results are displayed and Next,Previous buttons appear
-       squizAction.wait(GefSearch.pagination);
+       //squizAction.wait(GefSearch.pagination);
         // Need to do changes here
 
-        eyes.close();
+        
     }
 }
