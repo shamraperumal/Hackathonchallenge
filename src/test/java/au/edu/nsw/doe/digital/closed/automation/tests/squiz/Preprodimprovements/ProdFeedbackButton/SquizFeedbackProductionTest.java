a0f696180e03e9a001f3efc5a2af8de14cc77381
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-11-06 11:25:57 +1100

    refractored feedback test cases- moved test case back to ProdFeedbackButton

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/Preprodimprovements/ProdFeedbackButton/SquizFeedbackProductionTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/Preprodimprovements/ProdFeedbackButton/SquizFeedbackProductionTest.java
@@ -1,4 +1,4 @@
-package au.edu.nsw.doe.digital.closed.automation.tests.squiz.Preprodimprovements.Policy;
+package au.edu.nsw.doe.digital.closed.automation.tests.squiz.Preprodimprovements.ProdFeedbackButton;
 
 import au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz.GefSearch;
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
@@ -8,22 +8,22 @@ import org.junit.Test;
 public class SquizFeedbackProductionTest extends CapabilitiesBuilder {
 
     @Test
-    public void gefPoliciesDesktopPreProductionEyesTest() throws Exception {
+    public void FeedbackButtonTest() throws Exception {
 
         SquizActionMethods squizAction = new SquizActionMethods(driver);
         squizAction.getPage("gef-feedback-base", "gef-feedback-department-contacts");
         System.out.println("Page is launched succesfully");
-        Thread.sleep(2000);
+        Thread.sleep(8000);
 
         if ( squizAction.iswebelementpresent()==true)
             System.out.println("Feedback button is displayed. Test Passed");
         else
-            System.out.println("Feedback button is not displayed. Test failed");
+            System.out.println("Feedback button is not displayed. Test failed\n");
 
         squizAction.getPage("gef-feedback-base", "gef-feedback-parents-carers");
 
-        System.out.println("Search page is launched succesfully");
-        Thread.sleep(2000);
+        System.out.println("\nPArents and Carers page is launched succesfully");
+        Thread.sleep(8000);
 
         if ( squizAction.iswebelementpresent()==true)
             System.out.println("Feedback button is displayed. Test Passed");
