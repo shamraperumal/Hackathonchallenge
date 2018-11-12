d857fcbb07e6de5c6e5a03dde5f051ace1a60c2c
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-11-13 09:11:04 +1100

    Fixed feedback POM to use xpath instead of ID

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/FeedbackWidget.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/FeedbackWidget.java
@@ -6,5 +6,8 @@ public class FeedbackWidget {
 
     // Used to locate Feedback widget
     // 1. Clicking on this opens the feedback widget
-    public static By yourFeedbackWidget = By.id("single-navigation");
+  //  public static By yourFeedbackWidget = By.id("single-navigation");
+
+    public static By yourFeedbackWidget = By.xpath("//div[@id='single-navigation']");
+
 }
