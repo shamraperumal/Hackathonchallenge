d2713ecb67f904eabd88636f58434358166116cb
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-11-19 08:55:52 +1100

    additional logging

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/pageobjects/squiz/GefLongContent.java
@@ -0,0 +1,13 @@
+package au.edu.nsw.doe.digital.closed.automation.pageobjects.squiz;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import org.openqa.selenium.By;
+
+/* Created by Shravan Nagareddi on 16/11/2018
+
+ */
+public class GefLongContent   {
+
+    public static By leftIndex = By.xpath("//ul[@class='gef-link-list']");
+
+}
