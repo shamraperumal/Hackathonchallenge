b366f5db036c6f05a2d84bf8c9e58d716e92e9fb
shravannagareddi <shravan.nagareddi@det.nsw.edu.au>
2018-12-04 09:37:50 +1100

    Added Preprod test case

--- src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/ElasticSearchPreprod/PreprodTopFiftySearchedKeywordsTest.java
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/squiz/ElasticSearchPreprod/PreprodTopFiftySearchedKeywordsTest.java
@@ -1,6 +1,7 @@
 package au.edu.nsw.doe.digital.closed.automation.tests.squiz.ElasticSearchPreprod;
 
 import au.edu.nsw.doe.digital.closed.automation.setup.CapabilitiesBuilder;
+import au.edu.nsw.doe.digital.closed.automation.setup.LocalSetup;
 import au.edu.nsw.doe.digital.closed.automation.tests.squiz.SquizActionMethods;
 import org.junit.Test;
 
@@ -10,19 +11,22 @@ public class PreprodTopFiftySearchedKeywordsTest extends CapabilitiesBuilder {
 
         SquizActionMethods squizAction = new SquizActionMethods(driver);
 
-         String[] TopSearchedKeywords=squizAction.ListfromJenkins(System.getenv("keylist"));
+        String[] TopSearchedKeywords=squizAction.ListfromJenkins(System.getenv("keylist"));
 
-         for (int i=0;i<TopSearchedKeywords.length;i++)
+    //use below if you are running in local machine and not from jenkins
+//    String[] TopSearchedKeywords= {"Parramatta","child protection"};
+
+
+    for (int i=0;i<TopSearchedKeywords.length;i++)
          {
              System.out.println("Keyword is " + TopSearchedKeywords[i] );
              String url="/search?site=public_pre_education_nsw_gov_au&q=";
+             //https://pre.education.nsw.gov.au/search?site=public_pre_education_nsw_gov_au&q=pdp
              System.out.println("URL is " + url );
              squizAction.getPageElasticSearch("gef-education-homepage",url,TopSearchedKeywords[i]);
              Thread.sleep(6000);
-            eyes.checkWindow(System.getenv(TopSearchedKeywords[i]));
+           // eyes.checkWindow(System.getenv(TopSearchedKeywords[i]));
          }
-
-     
 
 
 }
