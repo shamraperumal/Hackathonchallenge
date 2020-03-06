e9047473968966006a514f221e67e2d9aa7191f7
DETNSW\sperumalamit <Shamra.PerumalAmith@det.nsw.edu.au>
2020-03-07 02:18:01 +1100

    test

---
+++ src/test/java/au/edu/nsw/doe/digital/closed/automation/tests/sws/swsfunctionaltest/CreateAlbum.java
@@ -0,0 +1,6 @@
+package au.edu.nsw.doe.digital.closed.automation.tests.sws.swsfunctionaltest;
+
+import au.edu.nsw.doe.digital.closed.automation.setup.*;
+
+public class CreateAlbum extends CapabilitiesBuilder {
+}
